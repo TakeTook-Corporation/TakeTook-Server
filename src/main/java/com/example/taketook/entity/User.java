31c4067c289f0ba85415f20428aa92045554a743
Abubakirov Vladimir <abubakirov04@mail.ru>
2023-01-19 16:36:51 +0300

    .

--- src/main/java/com/example/taketook/entity/User.java
+++ src/main/java/com/example/taketook/entity/User.java
@@ -20,7 +20,7 @@ public class User {
     private String city;
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL})
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE})
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user"),
             inverseJoinColumns = @JoinColumn(name = "role"))
